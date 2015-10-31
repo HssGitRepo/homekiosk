355e797368802396299bc1321cfe2f6e535aa712
wasim <wasimshariff001@gmail.com>
2015-11-01 05:29:00 +0530

    final commit

--- app/src/main/java/com/homekiosk/sample/homekiosk/GcmMessageHandler.java
+++ app/src/main/java/com/homekiosk/sample/homekiosk/GcmMessageHandler.java
@@ -4,6 +4,7 @@ package com.homekiosk.sample.homekiosk;
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -12,10 +13,13 @@ import com.google.android.gms.gcm.GoogleCloudMessaging;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Locale;
+
 
 public class GcmMessageHandler extends IntentService {
     static final String MSG_KEY = "m";
     String mes;
+    TextToSpeech tts;
     public static final int notifyID = 9001;
     public GcmMessageHandler() {
         super("GcmIntentService");
@@ -68,6 +72,9 @@ public class GcmMessageHandler extends IntentService {
 //        mNotifyBuilder.setAutoCancel(true);
 //        mNotificationManager.notify(notifyID, mNotifyBuilder.build());
         Log.e("GCM MESSAGE", "" + mes);
+        Intent i=new Intent(this,MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
         try {
             JSONObject root=new JSONObject(mes);
             String status=root.optString("status");
@@ -80,8 +87,112 @@ public class GcmMessageHandler extends IntentService {
                 Toast.makeText(getApplicationContext(),"Key database updated",Toast.LENGTH_SHORT).show();
 
             }
+            else
+            {
+                try {
+                    String doorstatus= root.optString("doorstatus");
+                    if (doorstatus.equals("1"))
+                    {
+                        tts= new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                            @Override
+                            public void onInit(int status) {
+                                if (status != TextToSpeech.ERROR) {
+                                    int result=  tts.setLanguage(Locale.getDefault());
+//                    tts.setPitch(1.0f);int result = tts.setLanguage(Locale.US);
+
+                                    if (result == TextToSpeech.LANG_MISSING_DATA
+                                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                        Toast.makeText(getApplicationContext(),"Voice not supported",Toast.LENGTH_LONG).show();
+                                    }
+                                    else {
+                                        tts.speak("Door Unlocked", TextToSpeech.QUEUE_FLUSH, null);
+                                        Toast.makeText(getApplicationContext(),"Door Unlocked",Toast.LENGTH_LONG).show();
+                                    }
+                                }
+
+                            }
+                        });
+
+                    }
+                    else
+                    {
+                        tts= new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                            @Override
+                            public void onInit(int status) {
+                                if (status != TextToSpeech.ERROR) {
+                                    int result=  tts.setLanguage(Locale.getDefault());
+//                    tts.setPitch(1.0f);int result = tts.setLanguage(Locale.US);
+
+                                    if (result == TextToSpeech.LANG_MISSING_DATA
+                                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                        Toast.makeText(getApplicationContext(),"Voice not supported",Toast.LENGTH_LONG).show();
+                                    }
+                                    else {
+                                        tts.speak("Door Locked", TextToSpeech.QUEUE_FLUSH, null);
+                                        Toast.makeText(getApplicationContext(),"Door Locked",Toast.LENGTH_LONG).show();
+                                    }
+                                }
+
+                            }
+                        });
+                    }
+                } catch (Exception e1) {
+                    Toast.makeText(getApplicationContext(),"GCM "+mes,Toast.LENGTH_LONG).show();
+                }
+            }
         } catch (JSONException e) {
-            e.printStackTrace();
+
+            try {
+                JSONObject root=new JSONObject(mes);
+                String doorstatus= root.optString("doorstatus");
+                if (doorstatus.equals("1"))
+                {
+                    tts= new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                        @Override
+                        public void onInit(int status) {
+                            if (status != TextToSpeech.ERROR) {
+                                int result=  tts.setLanguage(Locale.getDefault());
+//                    tts.setPitch(1.0f);int result = tts.setLanguage(Locale.US);
+
+                                if (result == TextToSpeech.LANG_MISSING_DATA
+                                        || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                    Toast.makeText(getApplicationContext(),"Voice not supported",Toast.LENGTH_LONG).show();
+                                }
+                                else {
+                                    tts.speak("Door Unlocked", TextToSpeech.QUEUE_FLUSH, null);
+                                    Toast.makeText(getApplicationContext(),"Door Unlocked",Toast.LENGTH_LONG).show();
+                                }
+                            }
+
+                        }
+                    });
+
+                }
+                else
+                {
+                    tts= new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                        @Override
+                        public void onInit(int status) {
+                            if (status != TextToSpeech.ERROR) {
+                                int result=  tts.setLanguage(Locale.getDefault());
+//                    tts.setPitch(1.0f);int result = tts.setLanguage(Locale.US);
+
+                                if (result == TextToSpeech.LANG_MISSING_DATA
+                                        || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                    Toast.makeText(getApplicationContext(),"Voice not supported",Toast.LENGTH_LONG).show();
+                                }
+                                else {
+                                    tts.speak("Door Locked", TextToSpeech.QUEUE_FLUSH, null);
+                                    Toast.makeText(getApplicationContext(),"Door Locked",Toast.LENGTH_LONG).show();
+                                }
+                            }
+
+                        }
+                    });
+                }
+            } catch (JSONException e1) {
+                Toast.makeText(getApplicationContext(),"GCM "+mes,Toast.LENGTH_LONG).show();
+            }
         }
 
     }
--- app/src/main/java/com/homekiosk/sample/homekiosk/MainActivity.java
+++ app/src/main/java/com/homekiosk/sample/homekiosk/MainActivity.java
@@ -3,11 +3,10 @@ package com.homekiosk.sample.homekiosk;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
@@ -15,6 +14,7 @@ import com.google.android.gms.gcm.GoogleCloudMessaging;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -22,6 +22,7 @@ public class MainActivity extends ActionBarActivity {
     String PROJECT_NUMBER = "344022689294";
     GoogleCloudMessaging gcm;
     String regid="";
+    TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +32,32 @@ public class MainActivity extends ActionBarActivity {
         Intent intent = new Intent("com.google.zxing.client.android.SCAN");
         intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
         startActivityForResult(intent, 0);
+
+
+    }
+
+
+
+    private void speakOut(final String text) {
+
+            tts= new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                  int result=  tts.setLanguage(Locale.getDefault());
+//                    tts.setPitch(1.0f);int result = tts.setLanguage(Locale.US);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Toast.makeText(getApplicationContext(),"Voice not supported",Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+                    }
+                }
+
+            }
+        });
 
 
     }
@@ -90,34 +117,56 @@ public class MainActivity extends ActionBarActivity {
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
 //                tvStatus.setText(intent.getStringExtra("SCAN_RESULT_FORMAT"));
-                Toast.makeText(getApplicationContext(), "" + intent.getStringExtra("SCAN_RESULT"), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "" + intent.getStringExtra("SCAN_RESULT"), Toast.LENGTH_SHORT).show();
                 DbHelper dbHelper=new DbHelper(MainActivity.this);
                 boolean isavailable=false;
                 List<String> keysavailable=new ArrayList<String>();
+                List<String> expiries=new ArrayList<String>();
                 keysavailable=dbHelper.getAllContacts();
+                expiries=dbHelper.getAllexpiries();
+                long expiry_date=0;
                 for (int i=0;i<keysavailable.size();i++)
                 {
                     Log.e("Result",keysavailable.get(i)+"__"+intent.getStringExtra("SCAN_RESULT"));
                     if (keysavailable.get(i).equalsIgnoreCase(""+intent.getStringExtra("SCAN_RESULT")))
                     {
                         isavailable=true;
+                        expiry_date=Long.parseLong(expiries.get(i));
                     }
                 }
                 if (isavailable==true)
                 {
-                    Toast.makeText(getApplicationContext(),"Authorized",Toast.LENGTH_SHORT).show();
+                    if (expiry_date<=System.currentTimeMillis()) {
+                        Toast.makeText(getApplicationContext(), "Authorized", Toast.LENGTH_SHORT).show();
+                        speakOut("Authorized, And its my pleasure to welcome you on behalf of my owner");
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "Your key has expired, Please contact the owner!"+expiry_date+":"+System.currentTimeMillis(), Toast.LENGTH_LONG).show();
+                        Log.e(""+expiry_date,""+System.currentTimeMillis());
+                        speakOut("Sorry, Your key has expired, Please contact the owner!");
+                    }
                 }
                 else
                 {
                     Toast.makeText(getApplicationContext(),"Authorization Denied",Toast.LENGTH_SHORT).show();
+                    speakOut("Sorry, Authorization Denied, Please contact the owner!");
                 }
             } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(getApplicationContext(), "Cancelled", Toast.LENGTH_SHORT).show();
             }
         }
         Intent intent1 = new Intent("com.google.zxing.client.android.SCAN");
         intent1.putExtra("SCAN_MODE", "QR_CODE_MODE");
         startActivityForResult(intent1, 0);
 
+    }
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
     }
 }
